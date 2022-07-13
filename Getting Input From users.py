dc62b71d17bc812bcbcca37e07e09c0b6fdd9b9e
Sean Birir <109024486+SeanBirir@users.noreply.github.com>
2022-07-13 08:25:22 +0300

    Add files via upload

---
+++ Getting Input From users.py
@@ -0,0 +1,11 @@
+#Getting input from users.
+#The user can put their own input into the program using input.
+
+input("what is your name : ")
+
+
+#You can put the input inside a variable and can do something with th variable.
+
+name = input("Enter your name :")
+age = input("Enter your age :")
+print("Hello " +  name + " ? you are " +age+ " years old.")
